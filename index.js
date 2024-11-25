3b0dd56849232e042e804b926fd40057f838df8d
vickey220500 <vickey220500@gmail.com>
2024-11-25 23:33:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const cors = require('cors');
-const routes = require('./routes/excelRoutes');
+const routes = require('./netlify/functions/routes/excelRoutes');
 app.use(cors())
 app.use(express.json())
 app.use('/api',routes);
