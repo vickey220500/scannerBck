375cd67da57e3736e68a20b619c3d4baba5e169d
vignesh <vickey220500@gmail.com>
2024-11-26 07:15:38 +0530

    restructure to netlify

---
+++ netlify/functions/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const routes = require('./routes/excelRoutes');
+app.use(cors())
+app.use(express.json())
+app.use('/api',routes);
+
+
+const PORT = 3000;
+app.listen(PORT,() => {
+    console.log(`Server is running on port ${PORT}`);
+});
